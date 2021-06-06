a4b807d0bdaf0930b184682bf48a4e2612841b30
Taiene Yuki <taieneyamazaki@gmail.com>
2021-06-06 18:22:46 -0300

    métodos cipher.encode/cipher.decode e dependentes

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,41 @@
 const cipher = {
-  // ...
+  
+  encode:
+    function (offset, string){
+      let code = "";
+      let i;
+      for (i=0; i < string.length; i++){
+        const ascii = string[i].charCodeAt();
+
+        if (ascii >= 65 && ascii<= 90){
+          code += String.fromCharCode(((ascii - 65 + offset)% 26) + 65);
+        }
+        else {
+          code += string[i];
+        }   
+      }
+      return code;
+    }, 
+
+  decode:
+    function (offset, string){
+      let decode = "";
+      let i;
+      for (i=0; i < string.length; i++){
+        const ascii = string[i].charCodeAt();
+        
+        if (ascii >= 65 && ascii<= 90){
+          decode += String.fromCharCode(((ascii - 90 - offset)% 26) + 90);
+        }
+        else {
+          decode += string[i];
+        }
+      }
+    return decode;
+  },  
+           
 };
+
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,43 @@ import cipher from './cipher.js';
 
 console.log(cipher);
 
+const toCipher = document.getElementById("string-input");
+const offset = document.getElementById("rot");
+const outputCode = document.getElementById("code");
 
 
+
+toCipher.onkeyup = function() {upperCipher()};
+function upperCipher() {
+  toCipher.value = toCipher.value.toUpperCase();
+  return toCipher.value;
+}
+
+document.getElementById("submitToCipher").addEventListener("click", startCipher);
+function  startCipher() {
+  return outputCode.innerHTML = cipher.encode(parseInt(offset.value), toCipher.value);
+}
+
+const decipher = document.getElementById("decipher");
+const outputMessage = document.getElementById("message");
+
+decipher.onkeyup = function() {upperDecipher()};
+function upperDecipher() {
+  decipher.value = decipher.value.toUpperCase();
+  return decipher.value;
+}
+
+document.getElementById("submitDecipher").addEventListener("click", startDecipher);
+function  startDecipher() {
+   return outputMessage.innerHTML = cipher.decode(parseInt(offset.value), decipher.value);
+  }
+  
+
+
+document.getElementById("clean").addEventListener("click", clean);
+function clean(){
+  document.getElementById("myForm").reset();
+  document.getElementById("code").innerHTML = "";
+  document.getElementById("message").innerHTML = "";
+  
+}
