a90dd22b4c9939ed911ab49231b68bd99e2be4e2
Taiene Yuki <taieneyamazaki@gmail.com>
2021-06-14 20:16:12 -0300

    Updates

--- cipher.js
+++ cipher.js
@@ -8,59 +8,79 @@ const cipher = {
 
       let code = "";
       let i;
-      while (i < string.length) {
+      for (i = 0; i < string.length; i++) {
         const ascii = string[i].charCodeAt();
 
-        if ((scii < 65 || ascii > 122) || (ascii > 90 && ascii < 122)) {
-          code += string[i];
-        } else if (offset < 0) {
+        if (offset < 0) {
           if (ascii >= 65 && ascii <= 90) {
             code += String.fromCharCode(((ascii - 90 + offset) % 26) + 90);
           }
           else {
-            code += String.fromCharCode(((ascii - 122 + offset) % 26) + 122);
+            if (ascii >= 97 && ascii <= 122) {
+              code += String.fromCharCode(((ascii - 122 + offset) % 26) + 122);
+            }
+            else {
+              code += string[i];
+            }
           }
-        } else if (ascii >= 65 && ascii <= 90) {
-          code += String.fromCharCode(((ascii - 65 + offset) % 26) + 65);
         }
         else {
-          code += String.fromCharCode(((ascii - 97 + offset) % 26) + 97);
+          if (ascii >= 65 && ascii <= 90) {
+            code += String.fromCharCode(((ascii - 65 + offset) % 26) + 65);
+          }
+          else {
+            if (ascii >= 97 && ascii <= 122) {
+              code += String.fromCharCode(((ascii - 97 + offset) % 26) + 97);
+            }
+            else {
+              code += string[i];
+            }
+          }
         }
       }
       return code;
     },
 
-  decode: function (offset, string) {
-    if (typeof offset !== "number" || typeof string !== "string") {
-      throw new TypeError("O valor é incompatível")
-    }
+  decode:
+    function (offset, string) {
+      if (typeof offset !== "number" || typeof string !== "string") {
+        throw new TypeError("O valor é incompatível")
+      }
 
-    let code = "";
-    let i;
-    while (i < string.length) {
-      const ascii = string[i].charCodeAt();
+      let decode = "";
+      let i;
+      for (i = 0; i < string.length; i++) {
+        const ascii = string[i].charCodeAt();
 
-      if ((scii < 65 || ascii > 122) || (ascii > 90 && ascii < 122)) {
-        code += string[i];
-      } else if (offset < 0) {
-        if (ascii >= 65 && ascii <= 90) {
-          decode += String.fromCharCode(((ascii - 65 - offset) % 26) + 65);
+        if (offset < 0) {
+          if (ascii >= 65 && ascii <= 90) {
+            decode += String.fromCharCode(((ascii - 65 - offset) % 26) + 65);
+          }
+          else {
+            if (ascii >= 97 && ascii <= 122) {
+              decode += String.fromCharCode(((ascii - 97 - offset) % 26) + 97);
+            }
+            else {
+              decode += string[i];
+            }
+          }
         }
         else {
-          decode += String.fromCharCode(((ascii - 97 - offset) % 26) + 97);
+          if (ascii >= 65 && ascii <= 90) {
+            decode += String.fromCharCode(((ascii - 90 - offset) % 26) + 90);
+          }
+          else {
+            if (ascii >= 97 && ascii <= 122) {
+              decode += String.fromCharCode(((ascii - 122 - offset) % 26) + 122);
+            }
+            else {
+              decode += string[i];
+            }
+          }
         }
-      } else if (ascii >= 65 && ascii <= 90) {
-        decode += String.fromCharCode(((ascii - 90 - offset) % 26) + 90);
       }
-      else {
-        decode += String.fromCharCode(((ascii - 122 - offset) % 26) + 122);
-      }
-    }
-    return code;
-  }
-
-
-
+      return decode;
+    },
 };
 
 export default cipher;
