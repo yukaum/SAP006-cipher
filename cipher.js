ce733e2682ca734356e68652b7c14bd3d558cdb1
Taiene Yuki <taieneyamazaki@gmail.com>
2021-06-14 19:50:56 -0300

    Updates

--- cipher.js
+++ cipher.js
@@ -8,79 +8,59 @@ const cipher = {
 
       let code = "";
       let i;
-      for (i = 0; i < string.length; i++) {
+      while (i < string.length) {
         const ascii = string[i].charCodeAt();
 
-        if (offset < 0) {
+        if ((scii < 65 || ascii > 122) || (ascii > 90 && ascii < 122)) {
+          code += string[i];
+        } else if (offset < 0) {
           if (ascii >= 65 && ascii <= 90) {
             code += String.fromCharCode(((ascii - 90 + offset) % 26) + 90);
           }
           else {
-            if (ascii >= 97 && ascii <= 122) {
-              code += String.fromCharCode(((ascii - 122 + offset) % 26) + 122);
-            }
-            else {
-              code += string[i];
-            }
+            code += String.fromCharCode(((ascii - 122 + offset) % 26) + 122);
           }
+        } else if (ascii >= 65 && ascii <= 90) {
+          code += String.fromCharCode(((ascii - 65 + offset) % 26) + 65);
         }
         else {
-          if (ascii >= 65 && ascii <= 90) {
-            code += String.fromCharCode(((ascii - 65 + offset) % 26) + 65);
-          }
-          else {
-            if (ascii >= 97 && ascii <= 122) {
-              code += String.fromCharCode(((ascii - 97 + offset) % 26) + 97);
-            }
-            else {
-              code += string[i];
-            }
-          }
+          code += String.fromCharCode(((ascii - 97 + offset) % 26) + 97);
         }
       }
       return code;
     },
 
-  decode:
-    function (offset, string) {
-      if (typeof offset !== "number" || typeof string !== "string") {
-        throw new TypeError("O valor é incompatível")
-      }
+  decode: function (offset, string) {
+    if (typeof offset !== "number" || typeof string !== "string") {
+      throw new TypeError("O valor é incompatível")
+    }
 
-      let decode = "";
-      let i;
-      for (i = 0; i < string.length; i++) {
-        const ascii = string[i].charCodeAt();
+    let code = "";
+    let i;
+    while (i < string.length) {
+      const ascii = string[i].charCodeAt();
 
-        if (offset < 0) {
-          if (ascii >= 65 && ascii <= 90) {
-            decode += String.fromCharCode(((ascii - 65 - offset) % 26) + 65);
-          }
-          else {
-            if (ascii >= 97 && ascii <= 122) {
-              decode += String.fromCharCode(((ascii - 90 - offset) % 26) + 90);
-            }
-            else {
-              decode += string[i];
-            }
-          }
+      if ((scii < 65 || ascii > 122) || (ascii > 90 && ascii < 122)) {
+        code += string[i];
+      } else if (offset < 0) {
+        if (ascii >= 65 && ascii <= 90) {
+          decode += String.fromCharCode(((ascii - 65 - offset) % 26) + 65);
         }
         else {
-          if (ascii >= 65 && ascii <= 90) {
-            decode += String.fromCharCode(((ascii - 90 - offset) % 26) + 90);
-          }
-          else {
-            if (ascii >= 97 && ascii <= 122) {
-              decode += String.fromCharCode(((ascii - 122 - offset) % 26) + 122);
-            }
-            else {
-              decode += string[i];
-            }
-          }
+          decode += String.fromCharCode(((ascii - 97 - offset) % 26) + 97);
         }
+      } else if (ascii >= 65 && ascii <= 90) {
+        decode += String.fromCharCode(((ascii - 90 - offset) % 26) + 90);
       }
-      return decode;
-    },
+      else {
+        decode += String.fromCharCode(((ascii - 122 - offset) % 26) + 122);
+      }
+    }
+    return code;
+  }
+
+
+
 };
 
 export default cipher;
--- index.js
+++ index.js
@@ -8,15 +8,15 @@ const outputMessage = document.getElementById("message");
 
 
 const slider = document.getElementById("slider");
-//const range =[-25,-24,-23,-22,-21,-20,-19,-18,-17,-16,-15,-14,-13,-12,-11,-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
+const range =[-33,-24,-23,-22,-21,-20,-19,-18,-17,-16,-15,-14,-13,-12,-11,-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
 
-let range = [];
+/*let range = [];
 for (let i=-25; i<26; i++){
   range.push(i);
   if (range[i] === 0){ 
     range.splice(i, 1); //(index, how many)
   }
-}
+}*/
 
 offset.innerHTML = range[offset.innerHTML.value]
 slider.oninput = function (){
@@ -41,3 +41,7 @@ function copy(){
   outputMessage.select();
   document.execCommand("copy");
 }
+
+
+console.log(input.value);
+console.log(offset.value);
