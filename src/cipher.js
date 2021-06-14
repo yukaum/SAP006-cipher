c54e6c7fe0c76744587f4a358283c84bc5cba9b7
Taiene Yuki <taieneyamazaki@gmail.com>
2021-06-14 11:46:40 -0300

    cipher/JS offset negativo_HTML janelas ladoa-a-lado_CSS backgroung +claro

--- src/cipher.js
+++ src/cipher.js
@@ -2,54 +2,85 @@ const cipher = {
   
   encode:
     function (offset, string){
-      if (typeof offset !== "number"){
-        throw new TypeError("O valor de offset não é um 'number'");
+      if (typeof offset !== "number" || typeof string !== "string"){
+        throw TypeError("O valor é incompatível") 
       } 
-      if (typeof string !== "string"){
-        throw new TypeError("O valor do texto não é uma 'string'");
-      } 
-      
+                
       let code = "";
       let i;
       for (i=0; i < string.length; i++){
         const ascii = string[i].charCodeAt();
-
-        if (ascii >= 65 && ascii<= 90){
-          code += String.fromCharCode(((ascii - 65 + offset)% 26) + 65);
-        }
+      
+        if (offset <0){
+          if (ascii >= 65 && ascii<= 90){
+            code += String.fromCharCode(((ascii - 90 + offset)% 26) + 90);
+          }
+          else { 
+            if (ascii >= 97 && ascii<= 122){
+              code += String.fromCharCode(((ascii - 122 + offset)% 26) + 122);
+            }
+            else {
+              code += string[i];
+            }   
+          }
+        } 
         else {
-          code += string[i];
-        }   
+          if (ascii >= 65 && ascii<= 90){
+            code += String.fromCharCode(((ascii - 65 + offset)% 26) + 65);
+          } 
+          else {
+            if (ascii >= 97 && ascii<= 122){
+              code += String.fromCharCode(((ascii - 97 + offset)% 26) + 97);
+            }
+            else {
+              code += string[i];
+            }   
+          }
+        }
       }
-      return code;
-    }, 
+    return code;
+  }, 
 
   decode:
     function (offset, string){
-      if (typeof offset !== "number"){
-        throw new TypeError("O valor de offset não é um 'number'");
-      } 
-      if (typeof string !== "string"){
-        throw new TypeError("O valor do texto não é uma 'string'");
-      } 
-      
+      if (typeof offset !== "number" || typeof string !== "string"){
+        throw new TypeError("O valor é incompatível")
+      }
+
       let decode = "";
       let i;
       for (i=0; i < string.length; i++){
         const ascii = string[i].charCodeAt();
         
-        if (ascii >= 65 && ascii<= 90){
-          decode += String.fromCharCode(((ascii - 90 - offset)% 26) + 90);
+        if (offset <0){
+          if (ascii >= 65 && ascii<= 90){
+            decode += String.fromCharCode(((ascii - 65 - offset)% 26) + 65);
+          }
+          else {
+            if (ascii >= 97 && ascii<= 122){
+              decode += String.fromCharCode(((ascii - 90 - offset)% 26) + 90);
+            }
+            else {
+              decode += string[i];
+            }   
+          }
         }
         else {
-          decode += string[i];
+          if (ascii >= 65 && ascii<= 90){
+            decode += String.fromCharCode(((ascii - 90 - offset)% 26) + 90);
+          }
+          else{
+            if (ascii >= 97 && ascii<= 122){
+              decode += String.fromCharCode(((ascii - 122 - offset)% 26) + 122);
+            }
+            else {
+              decode += string[i];
+            }
+          }
         }
       }
     return decode;
-  },  
-           
+  },       
 };
-
-
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -6,22 +6,38 @@ const input = document.getElementById("string-input");
 const offset = document.getElementById("rot");
 const outputMessage = document.getElementById("message");
 
-input.onkeyup = function() {upperCipher()};
-function upperCipher() {
-  input.value = input.value.toUpperCase();
-  return input.value;
+
+const slider = document.getElementById("slider");
+//const range =[-25,-24,-23,-22,-21,-20,-19,-18,-17,-16,-15,-14,-13,-12,-11,-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
+
+let range = [];
+for (let i=-25; i<26; i++){
+  range.push(i);
+  if (range[i] === 0){ 
+    range.splice(i, 1); //(index, how many)
+  }
 }
 
-document.getElementById("submitToCipher").addEventListener("click", startCipher);
+offset.innerHTML = range[offset.innerHTML.value]
+slider.oninput = function (){
+offset.value = range[slider.value];
+}    
+slider.oninput();
+
 function  startCipher() {
   return outputMessage.innerHTML = cipher.encode(parseInt(offset.value), input.value);
-}  
+}
+document.getElementById("submit-cipher").addEventListener("click", startCipher);  
   
 
-document.getElementById("submitDecipher").addEventListener("click", startDecipher);
 function  startDecipher() {
   return outputMessage.innerHTML = cipher.decode(parseInt(offset.value), input.value);
 }
-  
+document.getElementById("submit-decipher").addEventListener("click", startDecipher);  
 
- 
+
+document.getElementById("btn-copy").addEventListener("click", copy);
+function copy(){
+  outputMessage.select();
+  document.execCommand("copy");
+}
