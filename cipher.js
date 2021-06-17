ded75979acee06cb548440d429609ae7496a13cd
Taiene Yuki <taieneyamazaki@gmail.com>
2021-06-17 18:48:30 -0300

    Updates

--- cipher.js
+++ cipher.js
@@ -8,32 +8,27 @@ const cipher = {
 
       let code = "";
       let i;
+      
       for (i = 0; i < string.length; i++) {
         const ascii = string[i].charCodeAt();
 
-        if (offset < 0) {
-          if (ascii >= 65 && ascii <= 90) {
-            code += String.fromCharCode(((ascii - 90 + offset) % 26) + 90);
-          }
-          else {
-            if (ascii >= 97 && ascii <= 122) {
-              code += String.fromCharCode(((ascii - 122 + offset) % 26) + 122);
-            }
-            else {
-              code += string[i];
-            }
-          }
+        if (ascii < 65 || ascii > 122 || (ascii >90 && ascii < 97)) {
+          code += string[i];
         }
         else {
-          if (ascii >= 65 && ascii <= 90) {
-            code += String.fromCharCode(((ascii - 65 + offset) % 26) + 65);
-          }
-          else {
-            if (ascii >= 97 && ascii <= 122) {
-              code += String.fromCharCode(((ascii - 97 + offset) % 26) + 97);
+          if (offset < 0) {
+            if (ascii >= 65 && ascii <= 90) {
+              code += String.fromCharCode(((ascii - 90 + offset) % 26) + 90);
             }
             else {
-              code += string[i];
+              code += String.fromCharCode(((ascii - 122 + offset) % 26) + 122);
+            }
+          } else {
+            if (ascii >= 65 && ascii <= 90) {
+              code += String.fromCharCode(((ascii - 65 + offset) % 26) + 65);
+            }
+            else {
+              code += String.fromCharCode(((ascii - 97 + offset) % 26) + 97);
             }
           }
         }
@@ -44,7 +39,7 @@ const cipher = {
   decode:
     function (offset, string) {
       if (typeof offset !== "number" || typeof string !== "string") {
-        throw new TypeError("O valor é incompatível")
+        throw TypeError("O valor é incompatível")
       }
 
       let decode = "";
@@ -52,35 +47,29 @@ const cipher = {
       for (i = 0; i < string.length; i++) {
         const ascii = string[i].charCodeAt();
 
-        if (offset < 0) {
-          if (ascii >= 65 && ascii <= 90) {
-            decode += String.fromCharCode(((ascii - 65 - offset) % 26) + 65);
-          }
-          else {
-            if (ascii >= 97 && ascii <= 122) {
-              decode += String.fromCharCode(((ascii - 97 - offset) % 26) + 97);
-            }
-            else {
-              decode += string[i];
-            }
-          }
+        if (ascii < 65 || ascii > 122 || (ascii >90 && ascii < 97)) {
+          decode += string[i];
         }
         else {
-          if (ascii >= 65 && ascii <= 90) {
-            decode += String.fromCharCode(((ascii - 90 - offset) % 26) + 90);
-          }
-          else {
-            if (ascii >= 97 && ascii <= 122) {
-              decode += String.fromCharCode(((ascii - 122 - offset) % 26) + 122);
+          if (offset < 0) {
+            if (ascii >= 65 && ascii <= 90) {
+              decode += String.fromCharCode(((ascii - 65 - offset) % 26) + 65);
             }
             else {
-              decode += string[i];
+              decode += String.fromCharCode(((ascii - 97 - offset) % 26) + 97);
+            }
+          } else {
+            if (ascii >= 65 && ascii <= 90) {
+              decode += String.fromCharCode(((ascii - 90 - offset) % 26) + 90);
+            }
+            else {
+              decode += String.fromCharCode(((ascii - 122 - offset) % 26) + 122);
             }
           }
         }
       }
       return decode;
-    },
-};
+    }
+}
 
 export default cipher;
